cb32954c879346cf774220ab6ee373175667a4f8
lnfecteDru <nikitasmn98@gmail.com>
2023-04-07 12:10:11 +0300

    Added draw menu view and template

--- draw_menu_app/views.py
+++ draw_menu_app/views.py
@@ -1,3 +1,10 @@
+from typing import Optional
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def draw_menu_view(request, selected_node: Optional[str] = None):
+    return render(request, 'draw_menu_app/index.html', context={
+        'selected_node': selected_node,
+    })
